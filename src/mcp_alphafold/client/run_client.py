2a1ca690a34e067cf4808980cc67a435d0599207
zeinab-sheikhi <zeinab.sheykhie@gmail.com>
2025-04-29 10:17:48 +0200

    fixed the logger bug

--- src/mcp_alphafold/client/run_client.py
+++ src/mcp_alphafold/client/run_client.py
@@ -3,14 +3,16 @@ import logging
 
 from mcp_alphafold.client.mcp_client import MCPClient
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 logger = logging.getLogger(__name__)
 
-
-async def run():
-    # Create a stdio client
+async def run_client():
     client = MCPClient.stdio(
         command="python",
-        args=["/Users/zeinab/projects/mcp-alphafold/src/mcp_alphafold/server/run_server.py", "--transport", "stdio"],
+        args=["/Users/zsheikhitarghi/projects/mcp-alphafold/src/mcp_alphafold/server/run_server.py", "--transport", "stdio"],
     )
 
     try:
@@ -18,29 +20,27 @@ async def run():
         logger.info("Connected to MCP server successfully!")
 
         tools = await client.list_tools()
-        logger.info("\nAvailable tools:", len(tools))
+        logger.info(f"Available tools: {len(tools)}")
 
         result = await client.call_tool(
             tool_name="get_alphafold_prediction",
             qualifier="Q5VSL9",
         )
-        logger.info("\nTool result:\n", result)
+        logger.info(f"Tool result:\n{result.content[0].text}")
+        await asyncio.sleep(5)
 
     except Exception as e:
         logger.error(f"Error: {e}")
     finally:
-        # Always close the connection
         await client.cleanup()
-
 
 def main():
     try:
-        asyncio.run(run())
+        asyncio.run(run_client())
     except KeyboardInterrupt:
-        logger.error("\nScript interrupted by user")
+        logger.error("Script interrupted by user")
     except Exception as e:
         logger.error(f"Script error: {e}")
-
 
 if __name__ == "__main__":
     main()
